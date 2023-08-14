5aa80fe9d09707b5ab6b03474972e0a1dccf4c28
ytka <stasfyai73364@gmail.com>
2023-08-15 05:15:56 +0700

    add str_func

--- str_func.py
+++ str_func.py
@@ -1,7 +1,10 @@
 def caps():
+    """Не выключил caps lock"""
     line = input("Введите ваше предложение")
     print(line.upper())
 
 
-#    print(line.title())
-caps()
+def first_char_caps():
+    """Первая буква предложения будет заглавной"""
+    line = input("Введите ваше предложение")
+    print(line.title())
